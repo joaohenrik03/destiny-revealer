8835bcbc3ffdbdc20a4d4750d7a04ed3b4dece29
João Henrik <jh.signori@gmail.com>
2022-06-11 11:11:57 -0300

    update index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const response = document.querySelector('#response')
+
+const responses = [
+    "Certeza!",
+    "Não tenho tanta certeza.",
+    "É decididamente assim.",
+    "Não conte com isso.",
+    "Sem dúvidas!",
+    "Pergunte novamente mais tarde.",
+    "Sim, definitivamente!",
+    "Minha resposta é não.",
+    "Você pode contar com isso.",
+    "Melhor não te dizer agora.",
+    "A meu ver, sim.",
+    "Minhas fontes dizem não.",
+    "Provavelmente.",
+    "Não é possível prever agora.",
+    "Perspectiva boa.",
+    "As perspectivas não são tão boas.",
+    "Sim.",
+    "Concentre-se e pergunte novamente.",
+    "Sinais apontam que sim.",
+]
+
+function revealFate() {
+    const newResponsePosition = Math.floor(Math.random() * responses.length)
+
+    response.textContent = responses[newResponsePosition]
+}
+
+console.log(response)
